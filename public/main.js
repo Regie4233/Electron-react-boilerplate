0ba999d08d95b608bc7cbe78f1135636728ff226
Reginald Simon <96483953+Regie4233@users.noreply.github.com>
2024-02-04 17:47:14 -0500

    Added build function

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 const {app, BrowserWindow} = require('electron');
 require('@electron/remote/main').initialize();
+const path = require('path');
+const isDev = require('electron-is-dev');
 function createWindow(){
     const win = new BrowserWindow({
         width: 800,
@@ -8,7 +10,7 @@ function createWindow(){
             enableRemoteModule: true
         }
     });
-    win.loadURL('http://localhost:3000');
+    win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 }
 
 app.on('ready', createWindow);
